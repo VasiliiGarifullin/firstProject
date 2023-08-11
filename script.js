cfbb8816e887d0d89b4cef051a6d05cc748706a9
Vasilii <140749510+VasiliiGarifullin@users.noreply.github.com>
2023-08-11 18:58:30 +0400

    add new conditions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 "use strict";
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
@@ -10,14 +11,6 @@ const personalMovieDB = {
 	privat: false
 };
 
-// const a = prompt("Один из последних просмотренных фильмов?", ""),
-// 	b = prompt("На сколько оцените его?", ""),
-// 	c = prompt("Один из последних просмотренных фильмов?", ""),
-// 	d = prompt("На сколько оцените его?", "");
-      
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
 for(let i = 0; i < 2; i++) {
 	const a = prompt("Один из последних просмотренных фильмов?", ""),
 		  b = prompt("На сколько оцените его?", "");
@@ -29,6 +22,16 @@ for(let i = 0; i < 2; i++) {
 		console.log("error");
 		i--;
 	}
+}
+
+if (personalMovieDB.count < 10) {
+	console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.counts <= 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.counts > 30) {
+	console.log("Вы киноман")
+} else {
+	console.log("Произошла ошибка");
 }
 
 console.log(personalMovieDB);
